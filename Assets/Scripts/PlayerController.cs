3d95eb76573eb8d039dfcc1e8a8afecc96a8a75e
Shota Enomoto <29857@live.jp>
2018-10-11 16:58:59 +0900

    Main部分作成中

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    CharacterController controller;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    public float speedZ;
+    public float accelerationZ;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+        moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection * Time.deltaTime);
+	}
+}
